695241bf1992d04ebf26abe782a1e71f00c50eb3
Pavel <pavelzylkov25@gmail.com>
2021-05-12 18:46:12 +0300

    add lesson 6

---
+++ SQL/lesson6/lesson_6.sql
@@ -0,0 +1,17 @@
+--схема БД: https://docs.google.com/document/d/1NVORWgdwlKepKq_b8SPRaSpraltxoMg2SIusTEN6mEQ/edit?usp=sharing
+--colab/jupyter: https://colab.research.google.com/drive/1j4XdGIU__NYPVpv74vQa9HUOAkxsgUez?usp=sharing
+
+--task1  (lesson6)
+-- Компьютерная фирма: Создать таблицу all_products_with_index как объединение всех данных по ключу code (union all). Сделать индекс по полю type
+
+--task2  (lesson6)
+--Компьютерная фирма: Вывести список всех уникальных PC и производителя с ценой выше хотя бы одного принтера. Вывод: model, maker
+
+--task3  (lesson6)
+--Компьютерная фирма: Найдите номер модели продукта (ПК, ПК-блокнота или принтера), имеющего самую высокую цену. Вывести: model
+
+--task4  (lesson6)
+-- Компьютерная фирма: Создать таблицу all_products_with_index_task4 как объединение всех данных по ключу code (union all) и сделать флаг (flag) по цене > максимальной по принтеру. Сделать индекс по flag
+
+--task5  (lesson6)
+-- Компьютерная фирма: Создать таблицу all_products_with_index_task5 как объединение всех данных по ключу code (union all) и сделать флаг (flag) по цене > максимальной по принтеру. Также добавить нумерацию (через оконные функции) по каждой категории продукта в порядке возрастания цены (price_index). По этому price_index сделать индекс
